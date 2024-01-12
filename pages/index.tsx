5736d9363a9559a51f520d6c347270751c6c0030
Z <z@gam3.dev>
2024-01-11 22:26:55 -0800

    0.5.0 release

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,14 @@
-import { ThemeIcon, Text, Title, Container, SimpleGrid, rem, Image, Paper } from '@mantine/core'
+import {
+  ThemeIcon,
+  Text,
+  Title,
+  Container,
+  SimpleGrid,
+  rem,
+  Image,
+  Paper,
+  Alert,
+} from "@mantine/core";
 import {
   IconUser,
   IconFileImport,
@@ -7,57 +17,65 @@ import {
   IconPencil,
   IconStackFront,
   IconChargingPile,
-} from '@tabler/icons-react'
-import classes from './Welcome.module.css'
-import NextImage from 'next/image'
-import { IconChartBar } from '@tabler/icons-react'
-import { HowItWorks } from '../components/HowItWorks'
-import characters from '../public/screenshots/characters.png';
-import editCharacter from '../public/screenshots/editCharacter.png';
-import lorebooks from '../public/screenshots/lorebooks.png';
-import editLorebook from '../public/screenshots/editLorebook.png';
-import stories from '../public/screenshots/stories.png';
-import editStory from '../public/screenshots/editStory.png';
-
+  IconInfoCircle,
+} from "@tabler/icons-react";
+import classes from "./Welcome.module.css";
+import NextImage from "next/image";
+import { IconChartBar } from "@tabler/icons-react";
+import { HowItWorks } from "../components/HowItWorks";
+import characters from "../public/screenshots/characters.png";
+import editCharacter from "../public/screenshots/editCharacter.png";
+import lorebooks from "../public/screenshots/lorebooks.png";
+import editLorebook from "../public/screenshots/editLorebook.png";
+import stories from "../public/screenshots/stories.png";
+import editStory from "../public/screenshots/editStory.png";
 
 export const MOCKDATA = [
   {
     icon: IconUser,
-    title: '* Create characters',
+    title: "* Create characters",
     description: (
       <>
-        Easily create new characters from scratch by assembling from existing building blocks:{' '}
-        <b>personas, stories, lorebooks, and more</b>
+        Easily create new characters from scratch by assembling from existing
+        building blocks:{" "}
+        <b>personas, stories, lorebooks, model instructions and more</b>
       </>
     ),
   },
   {
     icon: IconPencil,
-    title: 'Stories',
+    title: "Stories",
     description: (
       <>
-        Wrap scenarios, first messages, dialogues and lorebooks into <b>stories</b> and share across characters with a
-        as few clicks as possible
+        Wrap scenarios, first messages, dialogues and lorebooks into{" "}
+        <b>stories</b> and share across characters with a as few clicks as
+        possible
       </>
     ),
   },
   {
     icon: IconDatabase,
-    title: 'Manage your data',
-    description: <>Easily edit, duplicate and organize characters, personas, scenarios, lorebooks and more</>,
+    title: "Manage your data",
+    description: (
+      <>
+        Easily edit, duplicate and organize characters, personas, scenarios,
+        lorebooks, model instructions and more
+      </>
+    ),
   },
   {
     icon: IconBook,
-    title: 'Lorebooks',
+    title: "Lorebooks",
     description: (
       <>
-        Manage and share lorebooks across characters. Import and export lorebooks to <b>.csv or .json</b>
+        Manage and share lorebooks across characters. Import and export
+        lorebooks to <b>.csv or .json</b>
       </>
     ),
   },
   {
     icon: IconFileImport,
-    title: '* Import and export',
+    title: "* Import and export",
     description: (
       <>
         Import and export characters to <i>.png</i> card format
@@ -66,15 +84,15 @@ export const MOCKDATA = [
   },
   {
     icon: IconChartBar,
-    title: '* Statistics',
+    title: "* Statistics",
     description: <>Explore, analyze and compare existing chats</>,
   },
-]
+];
 
 interface ItemProps {
-  icon: React.FC<any>
-  title: React.ReactNode
-  description: React.ReactNode
+  icon: React.FC<any>;
+  title: React.ReactNode;
+  description: React.ReactNode;
 }
 
 export function Item({ icon: Icon, title, description }: ItemProps) {
@@ -90,22 +108,45 @@ export function Item({ icon: Icon, title, description }: ItemProps) {
         {description}
       </Text>
     </div>
-  )
+  );
 }
 
 export default function IndexPage() {
-  const features = MOCKDATA.map((feature, index) => <Item {...feature} key={index} />)
+  const features = MOCKDATA.map((feature, index) => (
+    <Item {...feature} key={index} />
+  ));
 
   return (
     <Container className={classes.wrapper}>
-      <Paper withBorder shadow='sm' style={{ padding: 20 }} ta="center" fw={500} bg="var(--mantine-color-gray-1)">
-        Download latest (0.4.0) &nbsp;:&nbsp;
-        <a href="https://github.com/gam3dotdev/website/releases/latest/download/Charaday-Setup-0.4.0.exe">Windows</a>&nbsp;|&nbsp;
-        <a href="https://github.com/gam3dotdev/website/releases/latest/download/Charaday-0.4.0.dmg">Intel MacOS</a>&nbsp;|&nbsp;
-        <a href="https://github.com/gam3dotdev/website/releases/latest/download/Charaday-0.4.0-arm64.dmg">Apple Silicon M1 M2 M3 MacOS</a>
+      <Paper
+        withBorder
+        shadow="sm"
+        style={{ padding: 20 }}
+        ta="center"
+        fw={500}
+        bg="var(--mantine-color-gray-1)"
+      >
+        Download latest (0.5.0) &nbsp;:&nbsp;
+        <a href="https://github.com/gam3dotdev/website/releases/latest/download/Charaday-Setup-0.5.0.exe">
+          Windows
+        </a>
+        &nbsp;|&nbsp;
+        <a href="https://github.com/gam3dotdev/website/releases/latest/download/Charaday-0.5.0.dmg">
+          Intel MacOS
+        </a>
+        &nbsp;|&nbsp;
+        <a href="https://github.com/gam3dotdev/website/releases/latest/download/Charaday-0.5.0-arm64.dmg">
+          Apple Silicon M1 M2 M3 MacOS
+        </a>
       </Paper>
 
-      <Paper withBorder shadow='sm' style={{ padding: 20 }} bg="var(--mantine-color-gray-1)" mt="md">
+      <Paper
+        withBorder
+        shadow="sm"
+        style={{ padding: 20 }}
+        bg="var(--mantine-color-gray-1)"
+        mt="md"
+      >
         <Text size="md" className={classes.description}>
           An (unofficial)
         </Text>
@@ -117,11 +158,24 @@ export default function IndexPage() {
           </Text>
         </Container>
 
+        <Container size={560} mt="lg">
+          <Alert
+            variant="light"
+            color="orange"
+            title="Disclaimer"
+            icon={<IconInfoCircle />}
+          >
+            Charaday attaches and operates directly on Faraday database. It's
+            unlikely to cause any problems, but if you are paranoid - please
+            make a backup of your Faraday database before using Charaday.
+          </Alert>
+        </Container>
+
         <SimpleGrid
           mt={60}
           cols={{ base: 1, sm: 2, md: 3 }}
-          spacing={{ base: 'xl', md: 50 }}
-          verticalSpacing={{ base: 'xl', md: 50 }}
+          spacing={{ base: "xl", md: 50 }}
+          verticalSpacing={{ base: "xl", md: 50 }}
         >
           {features}
         </SimpleGrid>
@@ -131,21 +185,69 @@ export default function IndexPage() {
         </Text>
       </Paper>
 
-      <Paper withBorder shadow='sm' style={{ padding: 20 }} mt="md" bg="var(--mantine-color-gray-1)">
+      <Paper
+        withBorder
+        shadow="sm"
+        style={{ padding: 20 }}
+        mt="md"
+        bg="var(--mantine-color-gray-1)"
+      >
         <HowItWorks />
       </Paper>
 
-      <Paper withBorder shadow='sm' style={{ padding: 20 }} mt="md" bg="var(--mantine-color-gray-1)">
+      <Paper
+        withBorder
+        shadow="sm"
+        style={{ padding: 20 }}
+        mt="md"
+        bg="var(--mantine-color-gray-1)"
+      >
         <Title className={classes.title}>Screenshots</Title>
         <SimpleGrid>
-          <Image width="500" component={NextImage} src={characters} alt="characters" fit="contain"/>
-          <Image width="500" component={NextImage} src={editCharacter} alt="editCharacter" fit="contain"/>
-          <Image width="500" component={NextImage} src={stories} alt="stories" fit="contain"/>
-          <Image width="500" component={NextImage} src={editStory} alt="editStory" fit="contain"/>
-          <Image width="500" component={NextImage} src={lorebooks} alt="lorebooks" fit="contain"/>
-          <Image width="500" component={NextImage} src={editLorebook} alt="editLorebook" fit="contain"/>
+          <Image
+            width="500"
+            component={NextImage}
+            src={characters}
+            alt="characters"
+            fit="contain"
+          />
+          <Image
+            width="500"
+            component={NextImage}
+            src={editCharacter}
+            alt="editCharacter"
+            fit="contain"
+          />
+          <Image
+            width="500"
+            component={NextImage}
+            src={stories}
+            alt="stories"
+            fit="contain"
+          />
+          <Image
+            width="500"
+            component={NextImage}
+            src={editStory}
+            alt="editStory"
+            fit="contain"
+          />
+          <Image
+            width="500"
+            component={NextImage}
+            src={lorebooks}
+            alt="lorebooks"
+            fit="contain"
+          />
+          <Image
+            width="500"
+            component={NextImage}
+            src={editLorebook}
+            alt="editLorebook"
+            fit="contain"
+          />
         </SimpleGrid>
       </Paper>
     </Container>
-  )
+  );
 }
