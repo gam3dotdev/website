faa87fa7cf671529044df2b040a0ae16d1ae8b37
Z <z@gam3.dev>
2024-05-27 23:16:22 -0700

    Typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,26 +2,36 @@ import "@mantine/core/styles.css";
 import Head from "next/head";
 import { AppShell, Group, MantineProvider } from "@mantine/core";
 import { theme } from "../theme";
-import { IconLetterC, IconLetterH, IconLetterA, IconLetterR, IconLetterD, IconLetterY } from "@tabler/icons-react";
-import classes from './app.module.css'
-
+import {
+  IconLetterC,
+  IconLetterH,
+  IconLetterA,
+  IconLetterR,
+  IconLetterD,
+  IconLetterY,
+} from "@tabler/icons-react";
+import classes from "./app.module.css";
 
 export default function App({ Component, pageProps }: any) {
   return (
     <MantineProvider theme={theme}>
       <Head>
-        <title>GAM3.DEV | C H A R A D A Y</title>
+        <title>GAM3.DEV | Doghouse</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
         <link rel="shortcut icon" href="/icon.png" />
-        <script async src="https://us.umami.is/script.js" data-website-id="a376ddde-feeb-4564-a9df-53c949953c7b"></script>
+        <script
+          async
+          src="https://us.umami.is/script.js"
+          data-website-id="a376ddde-feeb-4564-a9df-53c949953c7b"
+        ></script>
       </Head>
 
       <AppShell header={{ height: 60 }} padding="md">
         <AppShell.Header className={classes.background}>
-          <Group h="100%" px="md" justify={'center'}>
+          <Group h="100%" px="md" justify={"center"}>
             <IconLetterC />
             <IconLetterH />
             <IconLetterA />
